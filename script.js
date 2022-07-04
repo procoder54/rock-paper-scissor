4588d2143054c80347348c37730e8308ad4d046a
Tarak Shaw <tarakspi11@gmail.com>
2022-07-04 14:54:01 +0530

    Changed the gameplay from console to UI

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
 // console.log('ha bhai!!!!!!!!!!')
-chance = ['rock','paper','scissor']
-function computerPlay(){
-   return chance[Math.floor(Math.random()*3)];
+chance = ['rock', 'paper', 'scissor']
+
+
+function computerPlay() {
+  return chance[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection){
-  if((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper'  && computerSelection === 'paper') || (playerSelection === 'scissor' && computerSelection === 'scissor')){
+
+function playRound(playerSelection, computerSelection) {
+  if ((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || (playerSelection === 'scissor' && computerSelection === 'scissor')) {
     return 'tie'
-  }else if(playerSelection === 'rock'){
-    if(computerSelection === 'scissor'){
+  } else if (playerSelection === 'rock') {
+    if (computerSelection === 'scissor') {
       return 'You won! Rock broke the Scissor..........'
-    }else{
+    } else {
       return 'You lost! Paper covered the Rock..........'
     }
-  }else if(playerSelection === 'paper'){
-    if(computerSelection === 'rock'){
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
       return 'You won! Paper covered the Rock..........'
-    }else{
+    } else {
       return 'You lost! Scissor cut the Paper..........'
     }
-  }else if(playerSelection === 'scissor'){
-    if(computerSelection === 'rock'){
+  } else if (playerSelection === 'scissor') {
+    if (computerSelection === 'rock') {
       return 'You lost! Rock broke the scissor...........'
-    }else{
+    } else {
       return 'You won! Scissor cut the Paper...........'
     }
   }
 }
 
-function game(){
-  let playerScore=0;
-  let computerScore=0;
-  for(let i=0; i<5; i++){
-    let playerSelection = prompt('Enter your change');
+
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+const div = document.createElement('div');
+let score = document.createElement('div')
+const result = document.createElement('div');
+buttons.forEach((button) => {
+  game: button.addEventListener('click', () => {
+    let playerSelection = button.id;
     let some = playRound(playerSelection, computerPlay());
-    console.log(some);
-    if(some.charAt(4) === 'w'){
+    div.textContent = some;
+    if (some.charAt(4) === 'w') {
       playerScore++;
-    }else{
+    } else if (some.charAt(4) === 'l') {
       computerScore++;
     }
-  }
-  if(playerScore>computerScore){
-    console.log('You won the match..............');
-  }else if(playerScore<computerScore){
-    console.log('You lost the match..............');
-  }else{
-    console.log('Game tie......................');
-  }
-}
-
-game();
-
-
-// computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
-// console.log(computerPlay());
+    score.textContent = `${playerScore.toString()}    ${computerScore.toString()}`;
+    body.appendChild(div);
+    body.appendChild(score);
+    if (playerScore == 5) {
+      result.textContent = 'You won the match..............';
+      body.appendChild(result);
+    } else if (computerScore == 5) {
+      result.textContent = 'You lost the match..............';
+      body.appendChild(result);
+    }
+  });
+});
